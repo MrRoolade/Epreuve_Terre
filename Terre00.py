a317ce00f6749bf5d7fd54deb36b40dbfdfc930c
MrRoolade <romain.recasens@free.fr>
2023-05-05 14:31:52 +0200

    L'alphabet

---
+++ Terre00.py
@@ -0,0 +1,13 @@
+##L’alphabet
+## affiche les lettres de l'alphabet puis revient à la ligne
+
+## Variables
+Lettre = ""
+Alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+Nbre_Lettre = len(Alphabet)
+
+for Lettre in range(Nbre_Lettre) :
+	print (Alphabet[Lettre] , end = '')
+print("")
+
+ #FIN
