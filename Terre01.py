a32b0d7675985f3798c51b461d782adf23cc655f
MrRoolade <romain.recasens@free.fr>
2023-05-05 15:40:44 +0200

    Nom du Programme

---
+++ Terre01.py
@@ -0,0 +1,14 @@
+##Nom du programme
+## affiche le nom du fichier du programme
+## importztion des Variables Systèmes
+import sys
+
+# for arg in sys.argv:
+# 	print (arg)
+
+print(sys.argv[0])
+
+
+
+
+ #FIN
