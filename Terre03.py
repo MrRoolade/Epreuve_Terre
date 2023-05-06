0d11d6b56b2126c699d40aae2dc3c2a7357bb05e
MrRoolade <romain.recasens@free.fr>
2023-05-06 15:48:22 +0200

    L'Alphabet à partir de V2

--- Terre03.py
+++ Terre03.py
@@ -8,18 +8,25 @@ Nbre_Lettre = len(Alphabet)
 import sys
 
 # passe l'argument saisi en minuscule
-Saisie = sys.argv[1].lower()
-
-# Test 
-if len(sys.argv)>= 3 : #si il n'y a pas trop d'arguments
-	print ("trop d'arg")
-elif len(sys.argv[1])>= 2 : #si l'argument contient plusieurs charactères
-	print ("arg trop long")
-elif Alphabet.count(Saisie) == 0: #si l'argument fait partie de la liste
-	print("votre saisie ne fait pas partie de l'alphabet")
+if len(sys.argv) == 1 : #si l'argument est absent
+	print ("veuillez saisir une Lettre de l'alphabet")
+	
 else:
-	for Lettre in range(Alphabet.index(Saisie), Nbre_Lettre) : #affiche les lettres de l'alphabet à partir d'un argument puis revient à la ligne
-		print (Alphabet[Lettre] , end = '')
-	print("")
+	Saisie = sys.argv[1].lower()
+
+	# Test 
+	if len(sys.argv)>= 3 : #si il n'y a pas trop d'arguments
+		print ("Veuillez saisir une seule Lettre svp")
+
+	elif len(sys.argv[1])>= 2 : #si l'argument contient plusieurs charactères
+		print ("Veuillez saisir une seule Lettre svp")
+
+	elif Alphabet.count(Saisie) == 0: #si l'argument fait partie de la liste
+		print("votre saisie ne fait pas partie de l'alphabet")
+
+	else:
+		for Lettre in range(Alphabet.index(Saisie), Nbre_Lettre) : #affiche les lettres de l'alphabet à partir d'un argument puis revient à la ligne
+			print (Alphabet[Lettre] , end = '')
+		print("")
 
 #FIN
